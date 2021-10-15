066966a29db2cac7ae25dd532c44c46d0a53b474
Fabrizio <fabrodaqp@gmail.com>
2021-10-15 16:49:03 -0500

    Update getAllFilesWithaSpecificExtension.py

--- getAllFilesWithaSpecificExtension.py
+++ getAllFilesWithaSpecificExtension.py
@@ -1,8 +1,12 @@
 #Import required Image library
 from PIL import Image, ImageFilter
 import glob, os
+path = "D:\\images-661-24-GB1.5"
+#path2 = "C:\\Users\\Fabrizio\\Desktop\\Todo de proyectos\\imagesbad\\pieza0\\pieza0background145" #Aca iria el directorio de el dataset
 
-path = "C:\\Users\\Fabrizio\\Desktop\\Todo de proyectos\\imagesbad\\pieza0\\pieza0background145" #Aca iria el directorio de el dataset
+num_piezas = 3
+num_bg = 661
+
 text_files = glob.glob(path + "/**/*.png", recursive = True)
 
 for image_path in text_files:
@@ -11,8 +15,8 @@ for image_path in text_files:
 	#OriImage.show()
 
 	#Applying GaussianBlur filter
-	#gaussImage = OriImage.filter(ImageFilter.GaussianBlur(51))
-	gaussImage = OriImage.filter(ImageFilter.BoxBlur(5))
+	gaussImage = OriImage.filter(ImageFilter.GaussianBlur(1.5))
+	#gaussImage = OriImage.filter(ImageFilter.BoxBlur(2))
 	#gaussImage.show()
 
 	#Save Gaussian Blur Image
